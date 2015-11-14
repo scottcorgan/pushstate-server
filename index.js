83c5503d0bd41e83b7c855800fc2b52084339903
Denis Shemanaev <denis.shemanaev@gmail.com>
2015-11-14 19:27:33 +0300

    Added configurable route file instead of hardcoded `/index.html`.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = connect();
 module.exports = {
   _port: 9000,
   _directory: 'public',
+  _file: '/index.html',
 
   port: function (port) {
     this._port = port;
@@ -16,15 +17,20 @@ module.exports = {
     this._directory = dir;
   },
 
+  file: function (file) {
+    this._file = dir;
+  },
+
   start: function (options) {
     options = options || {};
 
     var port = process.env.PORT || options.port || this._port;
     var directory = options.directory || this._directory;
     var directories = options.directories || [directory];
+    var file = options.file || this._file;
 
     app.use(modRewrite([
-      '!\\.html|\\.js|\\.json|\\.ico|\\.csv|\\.css|\\.less|\\.png|\\.svg|\\.eot|\\.ttf|\\.woff|\\.appcache|\\.jpg|\\.jpeg|\\.gif /index.html [L]'
+      '!\\.html|\\.js|\\.json|\\.ico|\\.csv|\\.css|\\.less|\\.png|\\.svg|\\.eot|\\.ttf|\\.woff|\\.appcache|\\.jpg|\\.jpeg|\\.gif ' + file + ' [L]'
     ]));
     app.use(compression());
 
