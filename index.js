c3f0d852a4b6be4afa19d286e2d998c57e44f8a0
Scott Corgan <scottcorgan@gmail.com>
2017-03-02 07:59:12 -0500

    Format with prettier

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-'use strict'
+"use strict";
 
-const connect = require('connect')
-const path = require('path')
-const serveStatic = require('serve-static')
-const serveStaticFile = require('connect-static-file')
-const compression = require('compression')
-const app = connect()
+const connect = require("connect");
+const path = require("path");
+const serveStatic = require("serve-static");
+const serveStaticFile = require("connect-static-file");
+const compression = require("compression");
+const app = connect();
 
-const PORT = 9000
-const DIRECTORY = 'public'
-const FILE = 'index.html'
-const HOST = '0.0.0.0'
+const PORT = 9000;
+const DIRECTORY = "public";
+const FILE = "index.html";
+const HOST = "0.0.0.0";
 
-exports.start = function (options, _onStarted) {
-  options = options || {}
+exports.start = function(options, _onStarted) {
+  options = options || {};
 
-  let port = options.port || process.env.PORT || PORT
-  let directory = options.directory || DIRECTORY
-  let directories = options.directories || [directory]
-  let file = options.file || FILE
-  let host = options.host || HOST
-  let onStarted = _onStarted || function () {}
+  let port = options.port || process.env.PORT || PORT;
+  let directory = options.directory || DIRECTORY;
+  let directories = options.directories || [directory];
+  let file = options.file || FILE;
+  let host = options.host || HOST;
+  let onStarted = _onStarted || function() {};
 
-  app.use(compression())
+  app.use(compression());
 
   // First, check the file system
   directories.forEach(directory =>
-    app.use(serveStatic(directory, { extensions: ['html'] })))
+    app.use(serveStatic(directory, { extensions: ["html"] })));
 
   // Then, serve the fallback file
-  app.use(serveStaticFile(path.join(directory, file)))
+  app.use(serveStaticFile(path.join(directory, file)));
 
   const server = app.listen(port, host, err =>
-    onStarted(err, server.address()))
+    onStarted(err, server.address()));
 
-  return server
-}
+  return server;
+};
--- test/index.js
+++ test/index.js
@@ -1,131 +1,130 @@
-let path = require('path')
-let test = require('tape')
-let got = require('got')
-let app = require('../index')
+let path = require("path");
+let test = require("tape");
+let got = require("got");
+let app = require("../index");
 
-test('server with port', t => {
-    let server
+test("server with port", t => {
+  let server;
 
-    server = app.start({
-        directory: path.join(__dirname, 'fixtures'),
-        port: 4321
-        }, err => {
+  server = app.start(
+    {
+      directory: path.join(__dirname, "fixtures"),
+      port: 4321
+    },
+    err => {
+      if (err) return server.close(() => t.end(err));
 
-        if (err) return server.close(() => t.end(err))
+      got("localhost:4321")
+        .then(res => {
+          t.equal(res.statusCode, 200, "root status code");
+          server.close(t.end.bind(t));
+        })
+        .catch(err => server.close(() => t.end(err)));
+    }
+  );
+});
 
-        got('localhost:4321')
-            .then(res => {
-                t.equal(res.statusCode, 200, 'root status code')
-                server.close(t.end.bind(t))
-            })
-            .catch(err => server.close(() => t.end(err)))
-    })
-})
+test("server with multiple directories", t => {
+  let server;
 
-test('server with multiple directories', t => {
-    let server
+  server = app.start(
+    {
+      directories: [
+        path.join(__dirname, "fixtures"),
+        path.join(__dirname, "test-directory")
+      ]
+    },
+    err => {
+      if (err) return server.close(() => t.end(err));
 
-    server = app.start({
-        directories: [
-            path.join(__dirname, 'fixtures'),
-            path.join(__dirname, 'test-directory')
-        ],
-        }, err => {
+      got("localhost:9000/test-json.json")
+        .then(res => {
+          t.equal(res.statusCode, 200, "response status code");
+          server.close(t.end.bind(t));
+        })
+        .catch(err => server.close(() => t.end(err)));
+    }
+  );
+});
 
-        if (err) return server.close(() => t.end(err))
+test("server with a custom host", t => {
+  let server;
+  const host = "0.0.0.0";
 
-        got('localhost:9000/test-json.json')
-            .then(res => {
-                t.equal(res.statusCode, 200, 'response status code')
-                server.close(t.end.bind(t))
-            })
-            .catch(err => server.close(() => t.end(err)))
-    })
-})
+  server = app.start(
+    {
+      host: host
+    },
+    (err, address) => {
+      if (err) return server.close(() => t.end(err));
 
-test('server with a custom host', t => {
-    let server
-    const host = '0.0.0.0';
+      got(`${address.address}:${address.port}/test-json.json`)
+        .then(res => {
+          t.equal(res.statusCode, 200, "response status code");
+          server.close(t.end.bind(t));
+        })
+        .catch(err => server.close(() => t.end(err)));
+    }
+  );
+});
 
-    server = app.start({
-        host: host
-      }, (err, address) => {
-        if (err) return server.close(() => t.end(err))
+test.skip("server with custom index file", t => {
+  let server;
 
-        got(`${address.address}:${address.port}/test-json.json`)
-            .then(res => {
-                t.equal(res.statusCode, 200, 'response status code')
-                server.close(t.end.bind(t))
-            })
-            .catch(err => server.close(() => t.end(err)))
-    })
-})
+  server = app.start(
+    {
+      directory: path.join(__dirname, "fixtures"),
+      file: "cat.js",
+      port: 8000
+    },
+    (err, address) => {
+      if (err) return server.close(() => t.end(err));
 
-test.skip('server with custom index file', t => {
-    let server
-
-    server = app.start({
-        directory: path.join(__dirname, 'fixtures'),
-        file: 'cat.js',
-        port: 8000
-      }, (err, address) => {
-
-        if (err) return server.close(() => t.end(err))
-
-        got(`localhost:${address.port}`)
-            .then(res => {
-                t.equal(res.statusCode, 200, 'root status code')
-                t.equal(res.body, "// cat.js", "body")
-                server.close(t.end.bind(t))
-            })
-            .catch(err => server.close(() => t.end(err)))
-    })
-})
+      got(`localhost:${address.port}`)
+        .then(res => {
+          t.equal(res.statusCode, 200, "root status code");
+          t.equal(res.body, "// cat.js", "body");
+          server.close(t.end.bind(t));
+        })
+        .catch(err => server.close(() => t.end(err)));
+    }
+  );
+});
 
 testWithServer("serves index.html", (t, done) => {
-    let base = got('localhost:9000')
-        .then(res =>{
-            t.equal(res.statusCode, 200, 'root status code')
-            t.equal(res.body, 'index.html', 'root response body')
-        })
+  let base = got("localhost:9000").then(res => {
+    t.equal(res.statusCode, 200, "root status code");
+    t.equal(res.body, "index.html", "root response body");
+  });
 
-    let somePath = got('localhost:9000/anything/here')
-        .then(res => {
-            t.equal(res.statusCode, 200, 'some path status code')
-            t.equal(res.body, 'index.html', 'some path response body')
-        })
+  let somePath = got("localhost:9000/anything/here").then(res => {
+    t.equal(res.statusCode, 200, "some path status code");
+    t.equal(res.body, "index.html", "some path response body");
+  });
 
-
-    Promise.all([base, somePath])
-        .then(() => done())
-        .catch(done)
-})
+  Promise.all([base, somePath]).then(() => done()).catch(done);
+});
 
 testWithServer("serves files", (t, done) => {
-    let tests = [
-        ["jpg", "image/jpeg"],
-        ["js", "application/javascript"],
-        ["json", "application/json"]
-    ].map(data =>
-        got(`localhost:9000/cat.${data[0]}`)
-            .then(res => {
-                t.equal(res.statusCode, 200, `${data[0]} successful`)
-                t.equal(res.headers['content-type'], data[1], `${data[0]} format`)
-            })
-    )
+  let tests = [
+    ["jpg", "image/jpeg"],
+    ["js", "application/javascript"],
+    ["json", "application/json"]
+  ].map(data => got(`localhost:9000/cat.${data[0]}`).then(res => {
+    t.equal(res.statusCode, 200, `${data[0]} successful`);
+    t.equal(res.headers["content-type"], data[1], `${data[0]} format`);
+  }));
 
-    Promise.all(tests)
-        .then(() => done())
-        .catch(done)
-})
+  Promise.all(tests).then(() => done()).catch(done);
+});
 
+function testWithServer(name, done) {
+  test(name, t => {
+    let server;
 
-function testWithServer (name, done) {
-    test(name, t => {
-        let server
-
-        server = app.start({directory: path.join(__dirname, 'fixtures')}, err =>
-            done(t, err => server.close(() => t.end(err)))
-        )
-    })
+    server = app.start(
+      { directory: path.join(__dirname, "fixtures") },
+      err => done(t, err => server.close(() => t.end(err)))
+    );
+  });
 }
